71010b5a7886db7ecebe3acf119ea463ac2df376
Tobias Sjösten <tobias.sjosten@gmail.com>
2012-08-13 22:08:08 +0200

    Added line parsing

--- src/Haborg.hs
+++ src/Haborg.hs
@@ -1,1 +1,51 @@
-module Haborg where
+module Haborg
+( Row(..)
+, Character(..)
+) where
+
+import Test.HUnit
+import Test.QuickCheck
+
+
+-- Row
+
+data Row = Prompt String | Line String
+
+readRow :: String -> Row
+readRow str = if isPrompt str then Prompt str else Line str
+
+isPrompt :: String -> Bool
+isPrompt str = isPrompt' False str
+
+isPrompt' :: Bool -> String -> Bool
+isPrompt' is "\255\249" = True
+isPrompt' is [] = False
+isPrompt' is (x:xs) = isPrompt' is xs
+
+parse :: Row -> String
+parse (Prompt row) = ""
+parse (Line row) = ""
+
+
+--- Character
+
+data Character = Character { health :: Int }
+
+
+-- Tests
+
+tests = TestList $ map TestCase [
+	assertEqual "Rows ending in GO AHEAD are prompts"
+		(isPrompt "row\255\249") True
+	, assertEqual "Rows ending in newline are not prompts"
+		(isPrompt "row\n") False
+	]
+
+prop_empty c1 = (c1::Int) == c1
+
+runTests = do
+	runTestTT tests
+	quickCheck prop_empty
+
+main :: IO ()
+main = runTests
